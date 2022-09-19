bdf5c53643940627ea9f01263d8c80e380101034
Emi <94977236+emichulo@users.noreply.github.com>
2022-09-19 19:06:43 +0300

    Add files via upload

--- randMenu.cpp
+++ randMenu.cpp
@@ -1,20 +1,32 @@
-// randMenu.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include<windows.h>
+#include<string>
+
+char mainMenu[3][50];
+
+size_t cursor;
+
+void SetColor(int k) {
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), k);
+
+};
 
 int main()
 {
+    system("### Ur nasty menu ###");
+
+    strcpy_s(mainMenu[0], 5, "#####");
+    strcpy_s(mainMenu[1], 5, "#####");
+    strcpy_s(mainMenu[2], 5, "#####");
+
+
+
+
+
+
+
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
