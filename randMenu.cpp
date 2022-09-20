e1f47c36814c16e1ab83b5876b991434d2f7a4ac
emichulo <emanuel.albu01@e-uvt.ro>
2022-09-20 14:30:20 +0300

    added the show menu function

--- randMenu.cpp
+++ randMenu.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include<windows.h>
-#include<string>
+#include <windows.h>
+#include <string.h>
 
 char mainMenu[3][50];
 
@@ -14,26 +14,54 @@ void SetColor(int k) {
 
 void assignment() {
 
-    strcpy_s(mainMenu[0], 5, "#####");
-    strcpy_s(mainMenu[1], 5, "#####");
-    strcpy_s(mainMenu[2], 5, "#####");
+    strcpy_s(mainMenu[0], 6, "aaaaa");
+    strcpy_s(mainMenu[1], 6, "bbbbb");
+    strcpy_s(mainMenu[2], 6, "ccccc");
 
-}
+};
 
+void showMenu() {
+
+    system("cls");
+    for (unsigned int i = 0; i < 3; i++) {
+        if (cursor == i)
+            SetColor(3);
+        else
+            SetColor(7);
+        std::cout << i + 1 << ". " << mainMenu[i] << "\n";
+    }
+    SetColor(7);
+
+};
 
 int main()
 {
-    system("### Ur nasty menu ###");
+    system("title Ur nasty menu ");
 
     assignment();
 
+    showMenu();
 
+    while (true) {
 
-
-
-
-
-    std::cout << "Hello World!\n";
+        if (GetAsyncKeyState(VK_LSHIFT))
+        {
+            Sleep(100);
+            if (cursor < 2)
+                cursor++;
+            else cursor = 0;
+            showMenu();
+        }
+        if (GetAsyncKeyState(VK_RSHIFT))
+        {
+            Sleep(100);
+            if (cursor > 0)
+                cursor--;
+            else cursor = 2;
+            showMenu();
+        }
+        
+    }
 }
 
 
