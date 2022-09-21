52541001b2c1d4eae2a417a5efbddcb625f3da19
emichulo <emanuel.albu01@e-uvt.ro>
2022-09-21 23:24:39 +0300

    added a console title function

--- Menu.h
+++ Menu.h
@@ -6,12 +6,15 @@
 #include<unordered_map>
 #include<functional>
 
-void setcolor(int k) {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), k);
-}
-
 class SubMenu {
 public:
+	SubMenu& addTitle(std::string title) {
+		std::wstring temp = std::wstring(title.begin(), title.end());
+		LPCWSTR tit;
+		tit = temp.c_str();
+		SetConsoleTitle(tit);
+		return *this;
+	}
 	SubMenu& addOption(std::string option, std::function<void()> funct) {
 		std::pair < std::string, std::function<void()>> p;
 		p.first = option;
@@ -19,60 +22,64 @@ public:
 		sm_data.push_back(p);
 		return *this;
 	}
-	size_t size()const { return sm_data.size(); }
-	std::pair < std::string, std::function<void()>>& operator[] (size_t index) { return sm_data[index]; }
-	SubMenu& operator++ () {
+	size_t size() const { return sm_data.size(); }
+	std::pair<std::string, std::function<void()>>& operator[] (size_t index) { return sm_data[index]; }
+	SubMenu& operator++() {
 		if (sm_index == sm_data.size() - 1)
 			sm_index = 0;
 		else
 			++sm_index;
 		return *this;
 	}
-	SubMenu& operator-- () {
+	SubMenu& operator--() {
 		if (sm_index == 0)
 			sm_index = sm_data.size()-1;
 		else
 			--sm_index;
 		return *this;
 	}
-	size_t getIndex()const { return sm_index; }
+	size_t getIndex() const { return sm_index; }
+	void print() {
+		system("cls");
+		for (size_t i = 0; i < this->size(); i++) {
+			if (i == this->getIndex())
+				setcolor(3);
+			else
+				setcolor(7);
+			std::cout << i << "." << (*this)[i].first << "\n";
+		}
+		setcolor(7);
+	}
 private:
 	std::vector<std::pair<std::string, std::function<void()>>> sm_data;
 	size_t sm_index = 0;
+
+	void setcolor(int k) {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), k);
+	}
 };
 
 class Menu {
 public:
+	Menu() {};
 	void addMenu(const std::string& name) {
 		m_data[name] = SubMenu{};
 	}
 	SubMenu& operator[] (const std::string& name) {return m_data.at(name);}
 
-	void print(SubMenu& sm) {
-		system("cls");
-		for (size_t i = 0; i < sm.size(); i++) {
-			if (i == sm.getIndex())
-				setcolor(3);
-			else
-				setcolor(7);
-			std::cout << i << "." << sm[i].first << "\n";
-		}
-		setcolor(7);
-	}
-
 	void run(const std::string& name) {
 		SubMenu& sm = m_data.at(name);
-		print(sm);
+		sm.print();
 		while (true) {
 			if (GetAsyncKeyState(VK_LSHIFT)) {
 				--sm;
 				Sleep(200);
-				print(sm);
+				sm.print();
 			}
 			if (GetAsyncKeyState(VK_RSHIFT)) {
 				++sm;
 				Sleep(200);
-				print(sm);
+				sm.print();
 			}
 			if (GetAsyncKeyState(VK_CONTROL)) {
 				Sleep(200);
--- randMenu.cpp
+++ randMenu.cpp
@@ -7,8 +7,16 @@ void init() {
 	obj.addMenu("Main Menu");
 	obj.addMenu("Secound Menu");
 
-	obj["Main Menu"].addOption("Go To Secound Menu", []() {obj.run("Secound Menu"); }).addOption("bbbb", []() {});
-	obj["Secound Menu"].addOption("Nothing", []() {}).addOption("Go To Main Menu", []() {obj.run("Main Menu"); }).addOption("Exit", []() {exit(-1); });
+	
+	obj["Main Menu"]
+		.addTitle("Main Menu")
+		.addOption("Go To Secound Menu", []() {obj.run("Secound Menu"); })
+		.addOption("bbbb", []() {});
+	obj["Secound Menu"]
+		.addTitle("Secound Menu")
+		.addOption("Nothing", []() {})
+		.addOption("Go To Main Menu", []() {obj.run("Main Menu"); })
+		.addOption("Exit", []() {exit(-1); });
 };
 
 int main() {
